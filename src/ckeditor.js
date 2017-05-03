0ef3f02211e185492f3ae0a134401db8db59d827
Timmy Garrabrant <timmygarrabrant@Timmys-iMac.local>
2017-05-03 10:27:40 -0700

    change readOnly after load

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class CKEditor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value,
-            config: props.config || {},
-            onChange: props.onChange
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+      config: props.config || {},
+      onChange: props.onChange
+    };
+  }
+
+  handleChange() {
+    this.state.onChange(this.state.value);
+  }
+
+  componentDidMount() {
+    if (!window.CKEDITOR) {
+      console.error("CKEditor not found");
+      return;
     }
 
-    handleChange() {
-        this.state.onChange(this.state.value)
+    this.instance = window.CKEDITOR.appendTo(
+      ReactDOM.findDOMNode(this),
+      this.state.config,
+      this.state.value
+    );
+    this.instance.on("change", () => {
+      this.state.value = this.instance.getData();
+      this.handleChange();
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("updating", this.state);
+  }
+
+  componentWillReceiveProps(props) {
+    if (!this.instance) {
+      return;
     }
 
-    componentDidMount() {
-        if (!window.CKEDITOR) {
-            console.error('CKEditor not found');
-            return;
-        }
-
-        this.instance = window.CKEDITOR.appendTo(ReactDOM.findDOMNode(this), this.state.config, this.state.value);
-        this.instance.on('change', () => {
-            this.state.value = this.instance.getData()
-            this.handleChange();
-        })
+    if (this.state.value !== props.value) {
+      // setData will move the cursor to the begining of the input
+      this.instance.setData(props.value);
     }
 
-    componentDidUpdate(){
-        console.log('updating', this.state);
-        
-    }
-   
-
-    componentWillReceiveProps(props) {
-        if(!this.instance){return}
-        
-        if (this.state.value !== props.value) {
-            // setData will move the cursor to the begining of the input
-            this.instance.setData(props.value)
-        }
-
-        this.setState({
-            value: props.value,
-            config: props.config || {},
-            onChange: props.onChange
-        })
-
+    if (this.state.config !== props.config) {
+      if ("readOnly" in props.config)
+        this.instance.setReadOnly(props.config.readOnly);
     }
 
-    render() {
-        return (<div></div>);
-    }
+    this.setState({
+      value: props.value,
+      config: props.config || {},
+      onChange: props.onChange
+    });
+  }
+
+  render() {
+    return <div />;
+  }
 }
-
 
 export default CKEditor;
